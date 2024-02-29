300d07af5398efed3dc323b592495b4660190278
Jamaymsc <jamay86@mail.ru>
2024-02-29 12:43:22 +0300

    valantis-app deploy v1.0

--- src/components/AppMain/AppMain.tsx
+++ src/components/AppMain/AppMain.tsx
@@ -1,18 +1,33 @@
 import { Box } from "@mui/material";
 import { ProductFilter } from "./components/ProductFilter";
 import { ProductList } from "./components/ProductList";
-import { Navigate, Route, Routes } from "react-router-dom";
-import { useState } from "react";
+import {
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export const AppMain = () => {
-
+  const location = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (location.pathname === "/valantis-app") {
+      navigate("products/1");
+    }
+  }, [location, navigate]);
   const [filters, setFilters] = useState<Record<string, any>>({});
   return (
     <Box>
       <ProductFilter onFilter={setFilters} />
       <Routes>
         <Route path="/" element={<Navigate to="/products/1" />} />
-        <Route path="/products/:page" element={<ProductList filters={filters} />} />
+        <Route
+          path="/products/:page"
+          element={<ProductList filters={filters} />}
+        />
       </Routes>
     </Box>
   );
